4198e1afec7720ff8e373b23890a3593af97eb05
Kamil Oleksiuk <koleksiuk20@gmail.com>
2016-01-04 18:27:34 +0100

    Add page scroll anchor

--- js/custom.js
+++ js/custom.js
@@ -7,12 +7,14 @@ $(document).ready(function(){
 	});
 
 	// Mobile menu
-
 	mobileMenu();
 
 	//Scrollr
-
 	var s = skrollr.init({forceHeight: false});
+
+	// Page scroll anchors init
+	initPageScrollLinks();
+
 	
 });
 
@@ -38,3 +40,39 @@ function mobileMenu() {
 		$('#main-nav-mobile').hide();
 	});
 }
+
+
+// Page scroll anchors
+function initPageScrollLinks(){
+	$('.page-scroll').on('click', function(event) {
+	    event.preventDefault();
+
+	    var target = $(event.target).attr('href'),
+	        elem = $('body').find(target);
+	        offset = elem.offset().top - 60;
+
+	    $('body, html').animate({scrollTop: offset}, 300);
+	});
+}
+
+
+/* $('#navigation a.link').on('click', function(event) {
+    $('html, body').animate({scrollTop: offset}, 300);
+    event.preventDefault();
+
+    var target = $(event.target).attr('href'),
+        elem = $('body').find(target);
+        offset = elem.offset().top - 60;
+
+    $('html, body').animate({scrollTop: offset}, 300);
+});
+
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+}); */
