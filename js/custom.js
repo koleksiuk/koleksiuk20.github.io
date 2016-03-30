101ed250e36c67d97239fa9f5462fe6ddfd94365
Kamil Oleksiuk <koleksiuk20@gmail.com>
2016-03-30 10:55:59 +0200

    first commit

--- js/custom.js
+++ js/custom.js
@@ -5,28 +5,61 @@ $(document).ready(function(){
 		interval: 5000
 	});
 
+	// Scrollr
+	var s = skrollr.init({forceHeight: false});
+
+	// Change active menu link on click
+	initShowActiveMenuLink();
+
+	// Show active menu link on scroll
+	//$(document).on("scroll", onScroll);
+
 	// Mobile menu
 	mobileMenu();
 
-	// Scrollr
-	var s = skrollr.init({forceHeight: false});
+	// Hide mobile menu on click init
+	initHideMainNavMobile();
 
 	// Page scroll anchors init
 	initPageScrollLinks();
 
-	// Hide mobile menu on click init
-	initHideMainNavMobile();
-	
 });
 
 // Header scroll
-$(window).bind('scroll', function(event) {
+/*$(window).bind('scroll', function(event) {
 	if($(window).scrollTop() > 100) {
 		$('.main-header').addClass('header-short');
 	} else {
 		$('.main-header').removeClass('header-short');
 	}	
-});
+});*/
+
+// Show active menu link on click
+function initShowActiveMenuLink() {
+	var menuLinkActive = $('.main-nav-list a');
+
+	menuLinkActive.on('click', function() {
+		menuLinkActive.removeClass('menu-link-active');
+		$(this).addClass('menu-link-active');
+	});
+}
+
+// Show active menu link on scroll
+/*function onScroll(event){
+	var scrollPos = $(document).scrollTop();
+	$('.main-nav-list a').each(function () {
+		console.log(this);
+		var currLink = $(this);
+		var refElement = $(currLink.attr("href"));
+		if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+			$('.nav-main ul li a').removeClass('menu-link-active');
+			currLink.addClass('menu-link-active');
+		}
+		else{
+			currLink.removeClass('menu-link-active');
+		}
+	});
+}*/
 
 // Mobile menu
 function mobileMenu() {
@@ -57,6 +90,6 @@ function initPageScrollLinks() {
 	        elem = $('body').find(target);
 	        offset = elem.offset().top - 60;
 
-	    $('body, html').animate({scrollTop: offset}, 300);
+	    $('body, html').animate({scrollTop: offset}, 500);
 	});
 }
